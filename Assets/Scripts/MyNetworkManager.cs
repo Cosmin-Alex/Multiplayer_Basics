aab0bf4acfc3d103b197da1a7f86bdf8ca30fc24
Cosmin Alex <brinzei.cosmin.alexandru@gmail.com>
2021-06-05 14:48:50 +0300

    Scripts: Added player name

--- Assets/Scripts/MyNetworkManager.cs
+++ Assets/Scripts/MyNetworkManager.cs
@@ -19,11 +19,18 @@ public class MyNetworkManager : NetworkManager
     }
 
     //Reference to the virtual function in NetworkManager and that we are going to override it
+    //This is when the Server adds a new player; conn is theyr connection
     public override void OnServerAddPlayer(NetworkConnection conn)
     {
         //Do your stuff - do your logic that you were ment to do
         base.OnServerAddPlayer(conn);
 
+        //As this will spawn the player; conn will get their connection, identity will get theyr identity using the NetworkIdentity component attached to it, and we can get ref to the networkPlayer
+        //This will allow us to call methods in MyNetworkPlayer script whenever a player is added
+        MyNetworkPlayer player = conn.identity.GetComponent<MyNetworkPlayer>();
+
+        //Set the display name - when you join, if your the first player, you will be called Player 1, and so fourth
+        player.SetDisplayName($"Player {numPlayers}");
         //So technically now first do what you normaly do (base.OnClientConnect(conn);) and then call the debug message
         Debug.Log("Server Log: A player has been added!");
 
@@ -35,7 +42,7 @@ public class MyNetworkManager : NetworkManager
 
         //The "$" at the begining the string changes the static string into a dinamic one - this is feature in C# - this lets us add a variable into the string
         //Used the {} in order to recognize the variable
-        Debug.Log($"Server Log: Number of players = {numPlayers}");
+        //Debug.Log($"Server Log: Number of players = {numPlayers}");
     }
 
     
---
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -0,0 +1,27 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyNetworkPlayer : NetworkBehaviour
+{
+    //If you put this before a field will sync var from the server to the clients
+    [SyncVar]
+    //Because we want to see it in the inspector
+    [SerializeField]
+    //Gave the default valie of "Missing Name" in case something goes wrong when setting a name, at least we will have a value that's telling us that the name is missing
+    private string displayName = "Missing Name";
+
+    //The displayName string is private and we need a public way to access it
+
+    //This method is only ever going to be run only on the server
+    //The Server is the only one who can do this - thus the [server] attribute
+    //This ism't necessary, we don't actually need this, but is to protect us from ourselfs, it will stop clients from running this method
+    //If we accidentally, somewhere else in our code, call this on the client, it won't even try to change the variable, it will stop it, because he is not the server
+    //And will put a warning in the console - kind of a protection
+    [Server]
+    public void SetDisplayName(string newDisplayName)
+    {
+        displayName = newDisplayName;
+    }
+}
