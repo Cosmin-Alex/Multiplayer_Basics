869395514711af9848ff4582fa8ee930bb2fbd5a
Cosmin Alex <brinzei.cosmin.alexandru@gmail.com>
2021-06-05 13:16:03 +0300

    Added the HUD back to NetworkManager GameObject; MyNetworManager script: when the client connects call a message

--- Assets/Scripts/MyNetworkManager.cs
+++ Assets/Scripts/MyNetworkManager.cs
@@ -5,5 +5,13 @@ using UnityEngine;
 
 public class MyNetworkManager : NetworkManager
 {
+    //Reference to the virtual function in NetworkManager and that we are going to override it
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        //Do your stuff - do your logic that you were ment to do
+        base.OnClientConnect(conn);
 
+        //So technically now first do what you normaly do (base.OnClientConnect(conn);) and then call the debug message
+        Debug.Log("I connected to a server!");
+    }
 }
